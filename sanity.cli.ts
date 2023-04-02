9250529d6172dfb70c04c634b834d22b7bcf141d
Zach Tatman <ztatman@Zachs-Air.attlocal.net>
2023-04-02 14:49:28 -0400

    update: add option for a 'testing' sanity dataset

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,10 +2,12 @@ import {defineCliConfig} from 'sanity/cli'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? '';
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET ?? '';
+const testingDataset = process.env.NEXT_PRIVATE_SANITY_DATASET ?? '';
+
 
 export default defineCliConfig({
   api: {
     projectId,
-    dataset
+    dataset: dataset
   }
 })
--- sanity.config.ts
+++ sanity.config.ts
@@ -5,13 +5,14 @@ import {schemaTypes} from './schemas'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? '';
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET ?? '';
+const testingDataset = process.env.NEXT_PRIVATE_SANITY_DATASET ?? '';
 
 export default defineConfig({
   basePath: "/studio",
   name: "Language_Travel_Adoptee_Studio",
   title: "Language Travel Adoptee Studio",
   projectId,
-  dataset,
+  dataset: dataset,
   plugins: [deskTool(), visionTool()],
   schema: {
     types: schemaTypes,
