2c48c7d0274dc6f92d0082ca34b03db7948f4a0c
ZTatman <zachariah.tatman@charter.com>
2023-04-21 13:25:53 -0400

    update: class names formatted and sorted

--- app/page.tsx
+++ app/page.tsx
@@ -17,13 +17,13 @@ export default function Home() {
             {/* Landing Page Text */}
             <div className="px-6 text-center text-white md:px-12">
               <h1 className="mb-3 font-display text-5xl font-bold leading-[1.15] md:text-6xl md:leading-[1.25] md:tracking-wider xl:text-7xl">
-                <span className="bg-clip-text text-transparent bg-gradient-to-b from-white from-50% to-transparent to-90%">
+                <span className="bg-gradient-to-b from-white from-50% to-transparent to-90% bg-clip-text text-transparent">
                   Language Travel
                   <br />
                   Adoptee
                 </span>
               </h1>
-              <p className="mx-auto mb-8 mt-0 font-sans text-xs tracking-wide xs:max-w-xs sm:max-w-sm md:max-w-xl md:text-sm text-white/85">
+              <p className="text-white/85 mx-auto mb-8 mt-0 font-sans text-xs tracking-wide xs:max-w-xs sm:max-w-sm md:max-w-xl md:text-sm">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. A iusto aspernatur rem pariatur delectus nemo
                 vitae itaque, quibusdam, harum ut consequatur sint id minima laboriosam architecto repellat! Deleniti,
                 et asperiores!
