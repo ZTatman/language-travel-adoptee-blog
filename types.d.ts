64d8d1d77ab0cc88d2e6aebc5c25d49e81a84789
ZTatman <zachariah.tatman@charter.com>
2023-05-26 18:44:45 -0400

    update: add types for blog posts and a global types.d.ts file

---
+++ types.d.ts
@@ -0,0 +1,63 @@
+type Base = {
+    _createdAt: string;
+    _id: string;
+    _rev: string;
+    _updatedAt: string;
+};
+
+interface Reference {
+    _ref: string;
+    _type: "reference";
+}
+
+interface Image {
+    _type: "image";
+    asset: Reference;
+}
+
+interface Slug {
+    _type: "slug";
+    current: string;
+}
+
+interface Span {
+    _key: string;
+    _type: "span";
+    marks: string[];
+    text: string;
+}
+
+interface Category extends Base {
+    title: string;
+    description: string;
+}
+
+interface Block {
+    _key: string;
+    _type: "block";
+    children: Span[];
+    markDefs: any[];
+    style: "normal" | "h1" | "h2" | "h3" | "h4" | "blockquote";
+}
+
+interface Title {
+    _type: "string";
+    asset: Reference;
+}
+
+interface Author extends Base {
+    name: string;
+    bio: Block[];
+    slug: Slug;
+    image: Image;
+}
+
+interface Post extends Base {
+    author: Author;
+    body: Block[];
+    categories: Category[];
+    mainImage: Image;
+    slug: Slug;
+    title: string;
+    description: string;
+}
